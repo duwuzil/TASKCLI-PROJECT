eb8a74d293a21e0b3fbcd178dfff583274306cd4
duwuzil <deuziljoestar@gmail.com>
2025-10-22 23:55:52 +0200

    modified:   Build/Makefile 	new file:   Build/bin/task-cli 	new file:   Build/obj/main.o 	new file:   Source/main.cpp 	new file:   Source/main.h 	new file:   Source/parser.cpp 	new file:   parser.h

---
+++ Source/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "main.h"
+
+int main(int argcount, char* arg[]) {
+    if(argcount <= 1){
+        std::cout << "error not enough arguments"
+        return 1;
+    }
+    
+}
